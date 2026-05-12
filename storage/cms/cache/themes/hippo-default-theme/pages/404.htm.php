f5a493659f670e82f2c3310ef2acb8ec60f03ac3
Tu Le <khuta441@gmail.com>
2026-05-12 16:43:14 +0700

    [fix] clean storage

--- storage/cms/cache/themes/hippo-default-theme/pages/404.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms6a013962c0817292573700_cb726530fe358e6b3aa067d077938ed1Class extends Cms\Classes\PageCode
-{
-
-}
--- storage/cms/cache/themes/hippo-default-theme/partials/footer.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms6a013962d6b30518837699_01a1b4c533aa96d74584cf880c012774Class extends Cms\Classes\PartialCode
-{
-
-}
